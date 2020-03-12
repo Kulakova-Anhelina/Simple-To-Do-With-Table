9a484e2422a4153f0624efbd2aef4d11f0ca9356
Kulakova-Anhelina <kulakovaanhelina@gmail.com>
2020-03-12 13:07:04 +0200

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import TodoList from "./components/TodoList";
 import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Todo List</header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Todo List</Typography>
+        </Toolbar>
+      </AppBar>
       <TodoList />
     </div>
   );
